e2673d81e09ca159b118849c9ac8b89c96b5360c
DiegoBrenesF <diegobrenesf@hotmail.com>
2024-12-05 13:52:38 -0600

    su aumenta en un elemento el arreglo grupo

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include "person.h"
 
 void mostrarArreglo (Person arreglo[], size_t tama){
-     printf ("\nArreglo de Personas:\n");
      printf ("%-20s %-10s %-10s\n", "Nombre", "Edad", "Altura(cm)");
      for (size_t i=0; i<tama; i++){
          printf ("%-20s %-10d %-10.2f\n", arreglo[i].name, arreglo[i].age, arreglo[i].height);
@@ -19,11 +18,26 @@ int main (){
            {"Andres Garita", 40, 172.31},
            {"Merry Popins", 30, 168.42},
            {"Lilliam Cervantes", 52, 166.33},
+           {"Elsa Figueroa", 61, 158.61},
+           {"Jona Soto", 26, 171.26},
+           {"Lorenzo Soto", 28, 169.28},
     };
 
     size_t tama = sizeof(grupo)/sizeof(grupo[0]);
 
-    printf ("\nArreglo original\n");
+    printf ("\nArreglo original de estructuras tipo Person\n");
+    mostrarArreglo (grupo, tama);
+
+    printf ("\nArreglo de estructuras tipo Person ordenado por nombre\n");
+    qsort(grupo, tama, sizeof(Person), compare_by_name);
+    mostrarArreglo (grupo, tama);
+
+    printf ("\nArreglo de estructuras tipo Person ordenado por edad\n");
+    qsort(grupo, tama, sizeof(Person), compare_by_age);
+    mostrarArreglo (grupo, tama);
+
+    printf ("\nArreglo de estructuras tipo Person ordenado por altura\n");
+    qsort(grupo, tama, sizeof(Person), compare_by_height);
     mostrarArreglo (grupo, tama);
 
     return 0;
